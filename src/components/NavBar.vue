96ce86a78855701eb7ec6a00b6cc5e1cbe0cb837
Daniel Heppner <daniel.heppner1@gmail.com>
2021-04-13 21:52:10 -0700

    working menu button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,30 +14,9 @@
               alt=""
             >
           </a>
-          <div class="text-xl font-display mx-4">
-            <span v-if="user">¡Hola, {{ user.email }}!</span>
-            <span
-              v-else
-              class="cursor-pointer"
-              @click="showLoginModal = true"
-            >Sign Up / Sign In</span>
-          </div>
         </div>
         <div class="flex items-center justify-evenly flex-grow lg:w-0">
-          <app-dropdown>
-            <template #toggler>
-              <div class="flex flex-row items-center font-display uppercase text-gray-300 mr-5">
-                <svg-icon
-                  type="mdi"
-                  :size="46"
-                  class="mx-2"
-                  :path="icons.mdiAccount"
-                />
-                User
-              </div>
-            </template>
-            <app-dropdown-list :items="accountOptions" />
-          </app-dropdown>
+          <user-menu-dropdown />
           <div class="flex flex-row items-center font-display uppercase text-gray-300">
             <svg-icon
               type="mdi"
@@ -46,6 +25,16 @@
               :path="icons.mdiVideo"
             />
             Schedule
+          </div>
+        </div>
+        <div class="flex justify-start items-center flex-shrink">
+          <div class="text-xl font-display mx-4">
+            <span v-if="user">¡Hola, {{ user.email }}!</span>
+            <span
+              v-else
+              class="cursor-pointer"
+              @click="showLoginModal = true"
+            >Sign Up / Sign In</span>
           </div>
         </div>
       </div>
@@ -58,13 +47,16 @@ import SvgIcon from '@jamescoyle/vue-icon'
 import { useAuth } from '@vueuse/firebase'
 import { mdiAccount, mdiVideo, mdiMenu } from '@mdi/js'
 import { ref } from 'vue'
-import AppDropdown from './dropdown/AppDropdown.vue'
-import AppDropdownList from './dropdown/AppDropdownList.vue'
 import LoginModal from './users/LoginModal.vue'
+import UserMenuDropdown from './dropdown/UserMenuDropdown.vue'
 
 export default {
   name: 'NavBar',
-  components: { SvgIcon, AppDropdown, AppDropdownList, LoginModal },
+  components: {
+    SvgIcon,
+    LoginModal,
+    UserMenuDropdown
+  },
   setup (props) {
     const showLoginModal = ref(false)
 
---
+++ src/components/dropdown/UserMenuDropdown.vue
@@ -0,0 +1,74 @@
+<template>
+  <Menu
+    as="div"
+    class="relative text-black"
+  >
+    <MenuButton class="flex flex-row items-center h-full font-display uppercase text-gray-300 mr-5">
+      <svg-icon
+        type="mdi"
+        :size="46"
+        class="mx-2"
+        :path="icons.mdiAccount"
+      />
+      User
+    </MenuButton>
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-out"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
+    >
+      <MenuItems
+        class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+      >
+        <div class="px-4 py-3">
+          <p class="text-sm leading-5">
+            Signed in as
+          </p>
+          <p class="text-sm font-medium leading-5 text-gray-900 truncate">
+            {{ user.email }}
+          </p>
+        </div>
+        <div class="py-1">
+          <MenuItem v-slot="{ active }">
+            <a
+              href="#account-settings"
+              :class="
+                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+              "
+              class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+            >
+              Account settings
+            </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+<script>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { useAuth } from '@vueuse/firebase'
+
+import { mdiAccount, mdiVideo, mdiMenu } from '@mdi/js'
+
+export default {
+  components: {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
+    SvgIcon
+  },
+  setup () {
+    const { isAuthenticated, user } = useAuth()
+    return {
+      user,
+      icons: { mdiAccount, mdiVideo, mdiMenu }
+    }
+  }
+}
+</script>
