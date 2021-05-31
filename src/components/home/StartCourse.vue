537926ac29aea2da78271b1df06fb3a4973a2ab1
mmbapin <bapinmm@gmail.com>
2021-06-01 01:23:32 +0600

    home review

--- src/components/home/StartCourse.vue
+++ src/components/home/StartCourse.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center bg-white pt-40 pb-40">
+  <div class="start-wrap text-center bg-white pt-40 pb-40">
     <a href="#" class="w-44 block b-rad m-auto">
       <p class="py-4 bg-pink-600 b-rad text-white">
         <span class="font-medium"> Start Here</span>
@@ -7,3 +7,12 @@
     </a>
   </div>
 </template>
+
+
+<style lang="css" scoped>
+.start-wrap {
+  background-image: url("/images/startss.png");
+  background-repeat: no-repeat;
+  background-position: top center;
+}
+</style>
