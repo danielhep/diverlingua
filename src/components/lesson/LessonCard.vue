b51a4209336b665d5dc50895d5085fc5d3240c35
Daniel Heppner <daniel.heppner1@gmail.com>
2021-05-31 20:07:26 -0700

    add youtube iframe

--- src/components/lesson/LessonCard.vue
+++ src/components/lesson/LessonCard.vue
@@ -2,8 +2,16 @@
 <template>
   <div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 w-8/12">
     <lesson-card-header :section="section" />
-    <div class="px-4 py-5 sm:p-6">
-      <!-- Content goes here -->
+    <div class="px-4 py-5 sm:p-6 flex justify-center">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/IMTYzQd9FUY"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
     </div>
     <div class="px-4 py-4 sm:px-6">
       <!-- Content goes here -->
